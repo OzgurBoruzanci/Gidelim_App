4a1f42d622dcd881dbed46d612d0a848b23a3ae3
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-07-15 20:00:06 +0300

    Flutter and Firebase connection completed.

--- bootcamp91/lib/main.dart
+++ bootcamp91/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:bootcamp91/theme/theme.dart';
 import 'package:bootcamp91/view/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 
   // Status bar rengini burada değiştiriyoruz
