2cd9cfc7371d6bc2fa30ca54bbb8b2e16e965e09
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-07-24 13:02:16 +0300

    Some UI debugged.

--- bootcamp91/lib/view/welcome_screen.dart
+++ bootcamp91/lib/view/welcome_screen.dart
@@ -19,67 +19,69 @@ class WelcomeScreenState extends State<WelcomeScreen> {
         // title: Text(ProjectTexts().projectName),
         automaticallyImplyLeading: false, // Geri butonunu gizler
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 8, left: 32, right: 32),
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 128.0),
-              child: Image.asset("assets/images/ic_welcome_image.png"),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: Text(
-                ProjectTexts().projectNameUppercase,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8, left: 32, right: 32),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 128.0),
+                child: Image.asset("assets/images/ic_welcome_image.png"),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Text(
+                  ProjectTexts().projectNameUppercase,
+                  style: const TextStyle(
+                      color: ProjectColors.whiteTextColor,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w800),
+                ),
+              ),
+              Text(
+                ProjectTexts().welcomeText,
                 style: const TextStyle(
-                    color: ProjectColors.whiteTextColor,
-                    fontSize: 50,
-                    fontWeight: FontWeight.w800),
+                  color: ProjectColors.whiteTextColor,
+                ),
               ),
-            ),
-            Text(
-              ProjectTexts().welcomeText,
-              style: const TextStyle(
-                color: ProjectColors.whiteTextColor,
+              // Text(
+              //   textAlign: TextAlign.center,
+              //   ProjectTexts().welcomeText,
+              //   style: Theme.of(context).textTheme.bodyLarge,
+              // ),
+              Padding(
+                padding: const EdgeInsets.only(top: 180.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            const SecondWelcomeScreen(),
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          var begin = const Offset(1.0, 0.0);
+                          var end = Offset.zero;
+                          var curve = Curves.ease;
+        
+                          var tween = Tween(begin: begin, end: end)
+                              .chain(CurveTween(curve: curve));
+        
+                          return SlideTransition(
+                            position: animation.drive(tween),
+                            child: child,
+                          );
+                        },
+                      ),
+                    ); //NAVIGATION FINISH HERE
+                  },
+                  child: Text(ProjectTexts().welcomeButtonText),
+                ),
               ),
-            ),
-            // Text(
-            //   textAlign: TextAlign.center,
-            //   ProjectTexts().welcomeText,
-            //   style: Theme.of(context).textTheme.bodyLarge,
-            // ),
-            Padding(
-              padding: const EdgeInsets.only(top: 180.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          const SecondWelcomeScreen(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        var begin = const Offset(1.0, 0.0);
-                        var end = Offset.zero;
-                        var curve = Curves.ease;
-
-                        var tween = Tween(begin: begin, end: end)
-                            .chain(CurveTween(curve: curve));
-
-                        return SlideTransition(
-                          position: animation.drive(tween),
-                          child: child,
-                        );
-                      },
-                    ),
-                  ); //NAVIGATION FINISH HERE
-                },
-                child: Text(ProjectTexts().welcomeButtonText),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
