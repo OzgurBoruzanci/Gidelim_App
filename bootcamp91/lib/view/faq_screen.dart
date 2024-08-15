a9324f0cc16440562a258933b8f11cafe4798702
Ozcan Bayram <ozzcanbayram@gmail.com>
2024-08-15 15:51:06 +0300

    FaqScreen strings edited.

--- bootcamp91/lib/view/faq_screen.dart
+++ bootcamp91/lib/view/faq_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bootcamp91/product/project_texts.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -16,60 +17,27 @@ class FAQScreen extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           _buildFAQTile(
-            question: 'Gidelim ne işe yarar?',
-            answer:
-                'Uygulama, kullanıcıların kafe menülerini kafelere gitmeden görüntülemesine ve kafeler için yorum yamasına, puan vermesine  olanak sağlar.\nEğer kendi kafeniz var ise, kafenizi uygulamada paylaşarak işletmenizi daha geniş kitlerlere ulaştırmanızı sağlar.',
-          ),
+              question: ProjectTexts().faq1, answer: ProjectTexts().ask1),
           _buildFAQTile(
-            question: 'Şifremi nasıl değiştirebilirim ?',
-            answer:
-                'Şifrenizi değiştirmek için uygulama menüsündeki Profil sayfasını açarak Parolayı Değiştir seçeneğini kullanabilirsiniz.',
-          ),
+              question: ProjectTexts().faq2, answer: ProjectTexts().ask2),
           _buildFAQTile(
-            question: 'Şifremi unutursam ne olur?',
-            answer:
-                'Şifrenizi unutursanız giriş yaparken ekranın altında görünen şifremi unuttum seçeneğini seçmelisiniz.\nArdından E-posta adresinizi girerek yeni bir parola belirlemek için E-posta hesabınıza gelen bağlantıyı kullanabilirsiniz.',
-          ),
+              question: ProjectTexts().faq3, answer: ProjectTexts().ask3),
           _buildFAQTile(
-            question: 'Favori kafelerim nerede görünür?',
-            answer:
-                'Favori kafeleriniz ana sayfanın en altında yer alan "Favorilerim" sekmesinde görünür.',
-          ),
+              question: ProjectTexts().faq4, answer: ProjectTexts().ask4),
           _buildFAQTile(
-            question: 'Yorumlar nasıl yapılır?',
-            answer:
-                'Yorumlarınızı kafeye tıklayınca açılan sayfada bulunan yorum bölümünden yapabilirsiniz.',
-          ),
+              question: ProjectTexts().faq5, answer: ProjectTexts().ask5),
           _buildFAQTile(
-            question: 'Kendi kafemi nasıl Gidelim\'e ekleyebilirim?',
-            answer:
-                'Sol menü üzerinden "Kafe Oluştur" butonuna tıklayarak, ardından Kafe Oluştur seçeneğini seçip kendi kafeni oluşturabilirsin. \nUNutma ki bir kullanıcı yalnıca bir kafe oluşturabilir.',
-          ),
+              question: ProjectTexts().ask6, answer: ProjectTexts().ask6),
           _buildFAQTile(
-            question: 'Kafemdeki ürünlerim neden görünmüyor?',
-            answer:
-                'Endişelenmeyin, sadece oluşturduğunuz kafenizi bizim onaylamamızı bekleyin. Tarafımızca kafeniz onaylandıktan sonra ürünleriniz görünür hale gelecektir.',
-          ),
+              question: ProjectTexts().faq7, answer: ProjectTexts().ask7),
           _buildFAQTile(
-            question: 'Oluşturduğum kafeyi silebilr miyim?',
-            answer:
-                'Kafenizi silmek için, sol menüde yer alan "Kafe Yönetimi" sekmesini açmalısınız. Ardından sayfanın sağ üst taraında yer alan çöp kutusu ile kafenizi onaylayarak silebilrisiniz. \nUnutmayın ki bu işlem geri alınamaz. ',
-          ),
+              question: ProjectTexts().faq8, answer: ProjectTexts().ask8),
           _buildFAQTile(
-            question: 'Avatarımı nasıl değiştirebilirim?',
-            answer:
-                'Avatarınızı sol menüde yer alan "Profil" sekmesindeki Avatarı Değiştir seçeneği ile kolayca değiştirebilirsiniz.',
-          ),
+              question: ProjectTexts().faq9, answer: ProjectTexts().ask9),
           _buildFAQTile(
-            question: 'Kafelerin puanları neye göre belirlenir?',
-            answer:
-                'Kafelerin puanları, kafe iççin yorum yapan kullanıcıların kafelere verdikleri puanların ortalamasına göre hesaplanır.',
-          ),
+              question: ProjectTexts().faq10, answer: ProjectTexts().ask10),
           _buildFAQTile(
-            question: 'Gitmek istediğim kafenin konumunu nasıl görebilirim?',
-            answer:
-                'Gitmek istediğiniz sayfanın konumunu görebilmek için, kafeye tıklayın ve alt menüde yer alan "Haritada Göster" butonuna tıklayın. Seçtiğiniz kafenin, size en yakın konumu harita üzerinden gösterilecektir. ',
-          ),
+              question: ProjectTexts().faq11, answer: ProjectTexts().ask11),
         ],
       ),
     );
